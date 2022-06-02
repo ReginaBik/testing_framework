148ccc33eee146e03d975057a5976ec24de83509
ReginaBik <reginabiktimirova1@gmail.com>
2022-06-02 18:27:08 -0500

    added soft and hard assertions

---
+++ src/test/java/testng_learning/_03_Soft_vs_Hard_Assertions.java
@@ -0,0 +1,33 @@
+package testng_learning;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+
+public class _03_Soft_vs_Hard_Assertions {
+
+    //Hard assertions
+    //when the is an error  at any line , the program flow will break, and it will not continue with the rest
+
+    @Test(priority = 1, description = "Practicing hard assertions")
+    public void method1(){
+        Assert.assertTrue(true);
+        Assert.assertTrue(false, "========Failure");
+
+        System.out.println("Hello World");
+    }
+
+    @Test(priority = 2, description = "practicing soft assertions")
+    public void method2(){
+        //Soft assertions
+        SoftAssert softAssert = new SoftAssert();
+        softAssert.assertTrue(true);
+        softAssert.assertTrue(false, "========Failure");
+
+        System.out.println("Hello world");
+    }
+
+
+
+
+}
