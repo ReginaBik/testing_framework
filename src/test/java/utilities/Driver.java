c1d909b4c10e1d721ae1f0a8abed6e1c3d7823e1
ReginaBik <reginabiktimirova1@gmail.com>
2022-06-06 18:51:36 -0500

    added conFig reader

--- src/test/java/utilities/Driver.java
+++ src/test/java/utilities/Driver.java
@@ -11,17 +11,24 @@ import java.util.concurrent.TimeUnit;
 
 public class Driver {
 
+    // 1. Private Constructor
     private Driver(){
 
     }
+
+    // 2. Private Driver
     private static WebDriver driver;
 
+    // 3. Get Driver and quit driver Method
     public static WebDriver getDriver(){
-        if(driver == null) { // means if there is no driver object, create one
-            // System.setProperty("webdriver.chrome.driver", "chromedriver"); here downloading chromedriver,...
+        if(driver == null){
+            // Telling your system where your chrome driver is located
+            //System.setProperty("webdriver.chrome.driver", "/Users/techglobal/IdeaProjects/selenium_intro/chromedriver");
 
-            String browser = "chrome"; //define which browser you will run your test in
-            switch(browser){
+            //The browser defined below with String is hard-coded
+            //String browser = "safari"; // define which browser you will run your test in
+
+            switch (ConfigReader.getProperty("browser")){
                 case "chrome":
                     WebDriverManager.chromedriver().setup();
                     driver = new ChromeDriver();
@@ -35,24 +42,25 @@ public class Driver {
                     driver = new SafariDriver();
                     break;
                 default:
-                    throw new NotFoundException("Browser IS NOT defined properly!!!");
+                    throw new NotFoundException("Browser IS NOT DEFINED properly!!!");
             }
             driver.manage().window().maximize();
-            driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+            driver.manage().timeouts().implicitlyWait(Long.parseLong(ConfigReader.getProperty("implicitWait")), TimeUnit.SECONDS);
         }
         return driver;
     }
 
     public static void quitDriver(){
+        try{
+            Thread.sleep(3000);
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
         if(driver != null){
-            driver.manage().deleteAllCookies(); // we want to delete all used password and id's, if not we can meet
-            // an issue: we would say program to login, but it would be already logged in
+            driver.manage().deleteAllCookies();
             driver.quit();
-            driver = null; // make sure driver is null!!! we definately quit it
+            driver = null;
         }
     }
-
-
-
-
 }
